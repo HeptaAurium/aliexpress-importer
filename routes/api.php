6ad8cc103de7e85243fbe75bf2fba916d05e60f1
ngugikiarie_ <ngugithe4th@gmail.com>
2024-09-22 14:49:24 +0300

    Routes fix

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,6 @@ use Heptaaurium\AliexpressImporter\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['token.from.url'])->group(function () {
-    Route::get('/', [AuthController::class, 'verifyToken']);
+    Route::get('/api', [AuthController::class, 'verifyToken']);
     Route::get('/aliexpress-importer/store-products/{productId}', [ProductsController::class, 'store']);
 });
