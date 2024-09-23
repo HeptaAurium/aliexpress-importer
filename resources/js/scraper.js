278e883711afc9dbe8b198f2d5b5c8a4f82db860
ngugikiarie_ <ngugithe4th@gmail.com>
2024-09-23 22:35:48 +0300

    BF: Error handling

--- resources/js/scraper.js
+++ resources/js/scraper.js
@@ -1,32 +1,34 @@
-// source: https://github.com/sudheer-ranga/aliexpress-product-scraper
-
 import puppeteer from "puppeteer";
-import * as cheerio from "cheerio";
-
-import { get as GetVariants } from "./utils/variants.js";
-import { get as GetReviews } from "./utils/reviews.js";
-import { get as GetShippingDetails } from "./utils/shipping.js";
 
 const getProduct = (productId) => async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     const url = `https://www.aliexpress.com/item/${productId}.html`;
-    data.logs.push("Navigating to " + url);
     const data = {
         logs: [],
         product: {},
     };
-    // Navigate the page to a URL.
-    await page.goto(url);
 
-    // Set screen size.
-    await page.setViewport({ width: 1080, height: 1024 });
+    try {
+        data.logs.push("Navigating to " + url);
+        await page.goto(url);
 
-    const pageTitle = await page.title();
-    data.logs.push("Page title: " + pageTitle);
-    data.product.title = pageTitle;
+        // Set screen size.
+        await page.setViewport({ width: 1080, height: 1024 });
+
+        const pageTitle = await page.title();
+        data.logs.push("Page title: " + pageTitle);
+        data.product.title = pageTitle;
+
+        // Additional scraping logic can be added here
+
+    } catch (error) {
+        data.logs.push("Error: " + error.message);
+    } finally {
+        await browser.close();
+    }
+
     return data;
-    await browser.close();
 };
 
 export { getProduct };
