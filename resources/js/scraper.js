01cdb7bd8c6f28763725b78ffc08c56e0ae86e7f
ngugikiarie_ <ngugithe4th@gmail.com>
2024-09-24 11:30:49 +0300

    BUG: Debugging logs

--- resources/js/scraper.js
+++ resources/js/scraper.js
@@ -1,7 +1,11 @@
 import puppeteer from "puppeteer";
 
 const getProduct = (productId) => async () => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+        headless: false,
+        devtools: true,
+        slowMo: 250, // slow down by 250ms
+    });
     const page = await browser.newPage();
     const url = `https://www.aliexpress.com/item/${productId}.html`;
     const data = {
@@ -10,8 +14,10 @@ const getProduct = (productId) => async () => {
     };
 
     try {
-        data.logs.push("Navigating to " + url);
-        await page.goto(url);
+        console.log("Navigating to " + url);
+        page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+
+        await page.goto(url, { waitUntil: 'networkidle2' });
 
         // Set screen size.
         await page.setViewport({ width: 1080, height: 1024 });
@@ -20,10 +26,19 @@ const getProduct = (productId) => async () => {
         data.logs.push("Page title: " + pageTitle);
         data.product.title = pageTitle;
 
+        const productTitle = await page.evaluate(() => {
+            return document.querySelector('h1.product-title').innerText;
+        });
+        data.product.title = productTitle;
+
+        await page.evaluate(() => {
+            debugger;
+        });
+
         // Additional scraping logic can be added here
 
     } catch (error) {
-        data.logs.push("Error: " + error.message);
+        data.logs.push("Error: " + error.stack);
     } finally {
         await browser.close();
     }
