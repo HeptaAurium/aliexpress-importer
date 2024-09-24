e89d0b993ad8793d7df4f021ddd8ac586bfe0042
ngugikiarie_ <ngugithe4th@gmail.com>
2024-09-24 13:08:55 +0300

    BF: Update api to match new aliexpress site

--- resources/js/scraper.js
+++ resources/js/scraper.js
@@ -6,6 +6,7 @@ const getProduct = (productId) => async () => {
         devtools: true,
         slowMo: 250, // slow down by 250ms
     });
+
     const page = await browser.newPage();
     const url = `https://www.aliexpress.com/item/${productId}.html`;
     const data = {
@@ -13,58 +14,43 @@ const getProduct = (productId) => async () => {
         product: {},
     };
 
-    const puppeteer = require('puppeteer');
+    // try {
+    console.log("Navigating to " + url);
+    data.logs.push("Navigating to " + url);
+    page.on('console', msg => console.log('PAGE LOG:', msg.text()));
 
-    async function scrapeProduct(productId) {
-        const browser = await puppeteer.launch({
-            headless: false,
-            devtools: true,
-            slowMo: 250, // slow down by 250ms
-        });
-        const page = await browser.newPage();
-        const url = `https://www.aliexpress.com/item/${productId}.html`;
-        const data = {
-            logs: [],
-            product: {},
-        };
+    await page.evaluate(() => console.log(`url is ${location.href}`));
+    await page.goto(url);
 
-        // try {
-        console.log("Navigating to " + url);
-        page.on('console', msg => console.log('PAGE LOG:', msg.text()));
+    // Set screen size.
+    await page.setViewport({ width: 1080, height: 1024 });
 
-        await page.evaluate(() => console.log(`url is ${location.href}`));
-        await page.goto(url);
+    const pageTitle = await page.title();
+    data.logs.push("Page title: " + pageTitle);
+    data.product.title = pageTitle;
 
-        // Set screen size.
-        await page.setViewport({ width: 1080, height: 1024 });
+    // Wait for the necessary DOM to be rendered
+    await page.waitForSelector('.product-price-current');
+    await page.waitForSelector('.product-description');
 
-        const pageTitle = await page.title();
-        data.logs.push("Page title: " + pageTitle);
-        data.product.title = pageTitle;
+    // Extract the product price
+    const price = await page.$eval('.product-price-current', el => el.innerText);
+    data.product.price = price;
 
-        // Wait for the necessary DOM to be rendered
-        await page.waitForSelector('.product-price-current');
-        await page.waitForSelector('.product-description');
+    // Extract the product description
+    const description = await page.$eval('.product-description', el => el.innerText);
+    data.product.description = description;
 
-        // Extract the product price
-        const price = await page.$eval('.product-price-current', el => el.innerText);
-        data.product.price = price;
+    data.logs.push("Product price: " + price);
+    data.logs.push("Product description: " + description);
 
-        // Extract the product description
-        const description = await page.$eval('.product-description', el => el.innerText);
-        data.product.description = description;
+    // } catch (error) {
+    //     data.logs.push("Error: " + error.message);
+    // } finally {
+    //     await browser.close();
+    // }
 
-        data.logs.push("Product price: " + price);
-        data.logs.push("Product description: " + description);
-
-        // } catch (error) {
-        //     data.logs.push("Error: " + error.message);
-        // } finally {
-        //     await browser.close();
-        // }
-
-        return data;
-    }
-};
+    return data;
+}
 
 export { getProduct };
